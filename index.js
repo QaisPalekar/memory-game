14aff65f69cc066a49217a4b51b96100a8ae3052
qaispalekar <qais@symboinsurance.com>
2020-05-23 20:32:34 +0530

    Upadted the Game UI\n * Changed the color of menu * added a check if you click on the card is visible * added steps count

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const colors = [
+const characters = [
     './jake-peralta.jpeg', 
     './amy.jpeg',
     './charles.jpeg',
     './terry.jpeg',
     './holt.jpeg',
     './rosa.jpeg',
-    './amy.jpeg',
-    './amy.jpeg',
-    './amy.jpeg'
 ]
+
+const gameVals = shuffleArray([...characters, ...characters]);
+let selectedValue = null;
+let steps = 0;
+let isTitleEnabled = true;
 
 function getRandomNumber(max) {
     return Math.floor(Math.random() * max);;
 }
+
 function shuffleArray(array) {
     let result = array;
     for (let i = 0; i < result.length; i++) {
@@ -21,41 +24,56 @@ function shuffleArray(array) {
     }
     return result;  
 }
-const gameVals = shuffleArray([...colors, ...colors]);
-let selectedValue = null;
-let points = 0;
-window.addEventListener("load", function(event) {
+
+function getDefaultTile(index) {
+    const tile =  this.document.createElement('img');
+    tile.className = 'tile';
+    tile.src = './default.jpeg';
+    tile.dataset.index = index;  
+    return tile;  
+}
+
+function startGame() {
     const board = document.getElementsByClassName('board')[0];
     gameVals.forEach((_ , index) => {
-        const tile =  this.document.createElement('img');
-        tile.className = 'tile';
-        tile.src = './default.jpeg';
-        tile.dataset.index = index;
-        board.appendChild(tile);
+        board.appendChild(getDefaultTile(index));
     });
-});
+    // board.style.visibility = 'visible';
+    document.getElementsByClassName('game')[0].style.visibility = 'visible';
+    document.getElementsByClassName('menu')[0].style.display = 'none';
+}
 
 window.addEventListener('click', event => {
     const target = event.target;
     if(event.target.classList.contains('tile')) {
+        const currentIndex = target.dataset.index;
+        console.log({currentIndex});
+        if(!isTitleEnabled || currentIndex === selectedValue) {
+            return;
+        }
         if(!selectedValue) {
-            selectedValue = target.dataset.index;
-            target.src = gameVals[target.dataset.index];
+            selectedValue = currentIndex;
+            target.src = gameVals[currentIndex];
         } else {
-            target.src = gameVals[target.dataset.index];
+            target.src = gameVals[currentIndex];
             setTimeout(() => {
-                if(gameVals[target.dataset.index] === gameVals[selectedValue]) {
-                    points += 1;
+                if(gameVals[currentIndex] === gameVals[selectedValue]) {
                     document.getElementsByClassName('success-prompt')[0].style.display = 'block';
+                    isTitleEnabled = false;
                     setTimeout(() => {
+                        isTitleEnabled = true;
                         document.getElementsByClassName('success-prompt')[0].style.display = 'none';
-                    }, 1500)
+                    }, 1000)
                 } else {
                     target.src = './default.jpeg';
+                    console.log(`[data-index="${selectedValue}"]`);
                     document.querySelector(`[data-index="${selectedValue}"]`).src = './default.jpeg';
                 }
                 selectedValue = null;
-            }, 1000)
+            }, 1000);
+            steps++;
+            document.querySelector('.steps').innerText = `Steps ${steps}`;
         }
+
     }
 });
